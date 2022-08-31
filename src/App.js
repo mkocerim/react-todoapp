50f21f3d5995fc4c7bc841df01bf29757be08c1a
mkocerim <mkocerim@gmail.com>
2022-08-31 21:06:16 +0300

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+
+
 
 function App() {
+  const [todoText, setTodoText]=useState("")
+  const handleSubmit=(event)=>{
+    event.preventDefault();
+    if(todoText===""){
+      alert("Todo text can't be empty!");
+      return;
+    }
+    console.log(todoText);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center my-5">Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="input-group mb-3">
+      <input 
+         value ={todoText}
+         type="text" 
+         className="form-control" 
+         placeholder="Type your Todo" 
+         onChange={(event)=>setTodoText(event.target.value)}
+      />
+         <button className="btn btn-primary" 
+         type="submit">
+          ADD
+        </button>
+        </div>
+      </form>
     </div>
   );
 }
