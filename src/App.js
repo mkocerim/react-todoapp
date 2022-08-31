0043bdd5ec3c377b3723e444d5bee371881ef9b4
mkocerim <mkocerim@gmail.com>
2022-08-31 22:18:02 +0300

    add todo and isdone

--- src/App.js
+++ src/App.js
@@ -4,6 +4,20 @@ import React,{useState} from "react";
 
 function App() {
   const [todoText, setTodoText]=useState("")
+  const[todos,setTodos]=useState([])
+  const changeISDone =(id)=>{
+    console.log(id);
+    const searchedTodo=todos.find((item)=>item.id ===id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter(item=>item.id !==id);
+
+    console.log(filteredTodos);
+    setTodos([updatedTodo,...filteredTodos]);
+
+  }
   const handleSubmit=(event)=>{
     event.preventDefault();
     if(todoText===""){
@@ -11,12 +25,28 @@ function App() {
       return;
     }
     console.log(todoText);
-  }
+  const hasTodos = todos.find(item=>item.text ===todoText)
+  console.log(hasTodos);
+  if(hasTodos !== undefined){
+    alert("You have the todo already")
+    return
+  };
+  const newTodo = {
+    id:new Date().getTime(),
+    isDone:false,
+    text:todoText,
+    date:new Date()
+  };
+
+  setTodos([newTodo,...todos]);
+  setTodoText("");
+  console.log(newTodo);
+  };
   return (
     <div className="container">
       <h1 className="text-center my-5">Todo App</h1>
       <form onSubmit={handleSubmit}>
-        <div className="input-group mb-3">
+        <div className= "input-group mb-3">
       <input 
          value ={todoText}
          type="text" 
@@ -30,6 +60,24 @@ function App() {
         </button>
         </div>
       </form>
+      {
+        todos.length <=0 ? (
+          <p className="text-centers my-5"> You don't have any todos yet</p>
+        ):
+        (
+     <React.Fragment>
+      {
+        todos.map(item =>(
+          <div className="alert alert-secondary d-flex justify-content-between align-items-center" role="alert">
+            <p>{item.text}</p>
+            <button onClick={()=>changeISDone(item.id)} className="btn btn-secondary btn-sm">{item.isDone===false ? "Done":"Undone"}</button>
+          </div>
+        ))
+      }
+     </React.Fragment>
+        )
+
+      }
     </div>
   );
 }
